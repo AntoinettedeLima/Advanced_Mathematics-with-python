e0b4d69ab177dfe51823b9827b442d016b1dcbdd
Antoinette de Lima <115338869+AntoinettedeLima@users.noreply.github.com>
2022-12-24 09:50:38 +0530

    Update 20210522_advanced_mathematics_cw.py

--- 20210522_advanced_mathematics_cw.py
+++ 20210522_advanced_mathematics_cw.py
@@ -7,39 +7,25 @@ Original file is located at
     https://colab.research.google.com/drive/15lgbFNqiHt8vb1S9h6Kotvy1ktbVE1Ws
 """
 
-#Qustion 1 : a)
+#Question 1:a)
 
-import math
 import sympy as sym
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import integrate
 
 x = sym.symbols('x')
 
-
 def f(x):
-    if - np.pi <= x < 0:
+    if 0 > x >= -np.pi:
         return x ** 2 + 1
     elif 0 <= x <= np.pi:
         return x * np.exp(-1)
 
+xValues = np.arange(-3 * np.pi, 3 * np.pi, 0.01)
+yValues = [f(x) for x in xValues]
 
-a0 = (1 / np.pi) * integrate.quad(f, -np.pi, np.pi)[0]
-
-a = [0]
-b = [0]
-
-for i in range(6):
-    a.append((2 / np.pi) * integrate.quad(lambda x: f(x) * np.cos(i * x), -np.pi, np.pi)[0])
-    b.append((2 / np.pi) * integrate.quad(lambda x: f(x) * np.sin(i * x), -np.pi, np.pi)[0])
-
-
-def f(x):
-    result = a0
-    for n in range(6):
-        result += a[n - 1] * np.cos(n * x) + b[n - 1] * np.sin(n * x)
-    return result
-
-
-print(f(5))
+plt.title("PF")
+plt.xlabel=("y")
+plt.ylabel=("x")
+plt.plot(xValues, yValues)
+plt.show()
