e5a113629a70e0afbe51a2a5e912751a7908f075
Antoinette de Lima <115338869+AntoinettedeLima@users.noreply.github.com>
2022-12-24 17:57:30 +0530

    Update 20210522_advanced_mathematics_cw.py

--- 20210522_advanced_mathematics_cw.py
+++ 20210522_advanced_mathematics_cw.py
@@ -29,3 +29,33 @@ plt.xlabel=("y")
 plt.ylabel=("x")
 plt.plot(xValues, yValues)
 plt.show()
+
+#Question 01:b)
+import math
+import numpy as np
+import sympy as sym
+from scipy import integrate
+import matplotlib.pyplot as plt
+
+x = sym.symbols('x')
+def f(x):
+    if - np.pi <= x < 0:
+        return x ** 2 + 1
+    elif 0 <= x <= np.pi:
+        return x * np.exp(-1)
+
+a0 = (1 / np.pi) * integrate.quad(f, -np.pi, np.pi)[0]
+a = [0]
+b = [0]
+
+for i in range(6):
+    a.append((2/np.pi) * integrate.quad(lambda x: f(x) * np.cos(i*x), -np.pi, np.pi)[0])
+    b.append((2/np.pi) * integrate.quad(lambda x: f(x) * np.sin(i*x), -np.pi, np.pi)[0])
+
+def f(x):
+    result = a0
+    for n in range(6):
+        result += a[n-1] * np.cos(n*x) + b[n-1] * np.sin(n*x)
+    return result
+
+print(f(4))
